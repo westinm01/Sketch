57216859e11b2c6ca55abe9f2c191a52b2cd1f91
myong003 <matthew.yong0801@gmail.com>
2021-12-03 17:43:18 -0800

    Added animations for Am getting hit in draw mode

--- Sketch/Assets/Scripts/AmScripts/Am_Movement.cs
+++ Sketch/Assets/Scripts/AmScripts/Am_Movement.cs
@@ -9,8 +9,9 @@ public class Am_Movement : MonoBehaviour
     public float horizontalSpeed;
     public float jumpHeight;
     public Animator anim;
-    public ChangePencilMode mode;
     [HideInInspector] public bool canJump = true;
+    private ChangePencilMode mode;
+    private AmCombat combat;
     private Vector2 m_Velocity = Vector2.zero;
 
     // Start is called before the first frame update
@@ -19,14 +20,20 @@ public class Am_Movement : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody2D>();
         anim = gameObject.GetComponent<Animator>();
         mode = gameObject.GetComponent<ChangePencilMode>();
+        combat = gameObject.GetComponent<AmCombat>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (gameObject.GetComponent<AmCombat>().isStunned()){
+            anim.SetBool("isStunned", true);
             return;
         }
+        else{
+            anim.SetBool("isStunned", false);
+        }
+        
         if (mode.canDraw){
             anim.SetBool("isDrawMode", true);
         }
