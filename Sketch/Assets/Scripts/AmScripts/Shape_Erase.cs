8a8edd13b099fe0ffe6aefdbc00271e9975e4fcd
brentnicks <brent_nicks@msn.com>
2022-01-03 17:24:29 -0800

    Update Shape_Erase.cs

--- Sketch/Assets/Scripts/AmScripts/Shape_Erase.cs
+++ Sketch/Assets/Scripts/AmScripts/Shape_Erase.cs
@@ -22,7 +22,7 @@ public class Shape_Erase : MonoBehaviour
         anim = gameObject.GetComponentInParent<Animator>();
         gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         map = GameObject.Find("Tilemap").GetComponent<Tilemap>();
-        verticalMap = GameObject.Find("Verticalmap").GetComponent<Tilemap>();
+        verticalMap = GameObject.Find("WallMap").GetComponent<Tilemap>();
         
     }
     private void Awake()
