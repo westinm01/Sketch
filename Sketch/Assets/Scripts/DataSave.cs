5ba342e7d8a91bc1070683c6411e5a751006b4e1
brentnicks <brent_nicks@msn.com>
2022-01-18 19:06:38 -0800

    7:06

--- Sketch/Assets/Scripts/DataSave.cs
+++ Sketch/Assets/Scripts/DataSave.cs
@@ -11,27 +11,48 @@ public static class DataSave
     public static void SaveData ()
     {
         BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/data.save";
+        string path1 = Application.persistentDataPath + "/data1.save";
+        string path2 = Application.persistentDataPath + "/data2.save";
+        string path3 = Application.persistentDataPath + "/data3.save";
 
-        FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
-        
-        FileInfo info = new System.IO.FileInfo(Application.persistentDataPath + "/data.save");
 
-        Data data = new Data(StaticInfo.levelInt, StaticInfo.levelBool, StaticInfo.health);
+        if (StaticInfo.saveProfle == 1)
+        {
+            FileStream stream = new FileStream(path1, FileMode.OpenOrCreate);
+            Data data = new Data(StaticInfo.levelInt, StaticInfo.levelBool, StaticInfo.health);
+            formatter.Serialize(stream, data);
+            stream.Close();
+        }
+        else if (StaticInfo.saveProfle == 2)
+        {
+            FileStream stream = new FileStream(path2, FileMode.OpenOrCreate);
+            Data data = new Data(StaticInfo.levelInt, StaticInfo.levelBool, StaticInfo.health);
+            formatter.Serialize(stream, data);
+            stream.Close();
+        }
+        else if (StaticInfo.saveProfle == 3)
+        {
+            FileStream stream = new FileStream(path3, FileMode.OpenOrCreate);
+            Data data = new Data(StaticInfo.levelInt, StaticInfo.levelBool, StaticInfo.health);
+            formatter.Serialize(stream, data);
+            stream.Close();
+        }
+
+        //FileInfo info = new System.IO.FileInfo(Application.persistentDataPath + "/data.save");
+
 
         //Debug.Log(data.levelInt[0].ToString() + ' ' + data.levelBool[0].ToString() + ' ' + data.levelBool[1].ToString() + ' ' + data.levelBool[2].ToString());
 
-       /* if (info.Length != 0)
-        {
-            data = formatter.Deserialize(stream) as Data;
-            Debug.Log(data.levelInt[0].ToString() + ' ' + data.levelBool[0].ToString() + ' ' + data.levelBool[1].ToString() + ' ' + data.levelBool[2].ToString());
-            data.levelBool[currentLevel - 1] = true;
-            data = RefreshData(data);
-            Debug.Log("file has pre-existing data");
-        }*/
+        /* if (info.Length != 0)
+         {
+             data = formatter.Deserialize(stream) as Data;
+             Debug.Log(data.levelInt[0].ToString() + ' ' + data.levelBool[0].ToString() + ' ' + data.levelBool[1].ToString() + ' ' + data.levelBool[2].ToString());
+             data.levelBool[currentLevel - 1] = true;
+             data = RefreshData(data);
+             Debug.Log("file has pre-existing data");
+         }*/
 
-        formatter.Serialize(stream, data);
-        stream.Close();
+
 
         //Debug.Log(DataSave.LoadData().levelInt[0].ToString()+ ' ' + DataSave.LoadData().levelBool[0].ToString() + ' ' + DataSave.LoadData().levelBool[1].ToString() + ' ' + DataSave.LoadData().levelBool[2].ToString());
     }
@@ -39,24 +60,72 @@ public static class DataSave
     public static void LoadData ()
     {
 
-        string path = Application.persistentDataPath + "/data.save";
-        if (File.Exists(path))
+        string path1 = Application.persistentDataPath + "/data1.save";
+        string path2 = Application.persistentDataPath + "/data2.save";
+        string path3 = Application.persistentDataPath + "/data3.save";
+
+        if (StaticInfo.saveProfle == 1)
         {
-            BinaryFormatter formatter = new BinaryFormatter();
-            FileStream stream = new FileStream(path, FileMode.Open);
-            Data data = formatter.Deserialize(stream) as Data;
-            //Debug.Log(data.levelInt[0].ToString() + ' ' + data.levelBool[0].ToString() + ' ' + data.levelBool[1].ToString() + ' ' + data.levelBool[2].ToString());
+            if (File.Exists(path1))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(path1, FileMode.Open);
+                Data data = formatter.Deserialize(stream) as Data;
+                //Debug.Log(data.levelInt[0].ToString() + ' ' + data.levelBool[0].ToString() + ' ' + data.levelBool[1].ToString() + ' ' + data.levelBool[2].ToString());
 
-            Array.Copy(data.levelInt, StaticInfo.levelInt, data.levelInt.Length);
-            Array.Copy(data.levelBool, StaticInfo.levelBool, data.levelBool.Length);
-            StaticInfo.health = data.health;
+                Array.Copy(data.levelInt, StaticInfo.levelInt, data.levelInt.Length);
+                Array.Copy(data.levelBool, StaticInfo.levelBool, data.levelBool.Length);
+                StaticInfo.health = data.health;
 
-            stream.Close();
+                stream.Close();
 
+            }
+            else
+            {
+                Debug.LogError("Save file not found in " + path1);
+            }
         }
-        else
+        else if (StaticInfo.saveProfle == 2)
         {
-            Debug.LogError("Save file not found in " + path);
+            if (File.Exists(path2))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(path2, FileMode.Open);
+                Data data = formatter.Deserialize(stream) as Data;
+                //Debug.Log(data.levelInt[0].ToString() + ' ' + data.levelBool[0].ToString() + ' ' + data.levelBool[1].ToString() + ' ' + data.levelBool[2].ToString());
+
+                Array.Copy(data.levelInt, StaticInfo.levelInt, data.levelInt.Length);
+                Array.Copy(data.levelBool, StaticInfo.levelBool, data.levelBool.Length);
+                StaticInfo.health = data.health;
+
+                stream.Close();
+
+            }
+            else
+            {
+                Debug.LogError("Save file not found in " + path2);
+            }
+        }
+        else if (StaticInfo.saveProfle == 3)
+        {
+            if (File.Exists(path3))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(path3, FileMode.Open);
+                Data data = formatter.Deserialize(stream) as Data;
+                //Debug.Log(data.levelInt[0].ToString() + ' ' + data.levelBool[0].ToString() + ' ' + data.levelBool[1].ToString() + ' ' + data.levelBool[2].ToString());
+
+                Array.Copy(data.levelInt, StaticInfo.levelInt, data.levelInt.Length);
+                Array.Copy(data.levelBool, StaticInfo.levelBool, data.levelBool.Length);
+                StaticInfo.health = data.health;
+
+                stream.Close();
+
+            }
+            else
+            {
+                Debug.LogError("Save file not found in " + path3);
+            }
         }
     }
 }
