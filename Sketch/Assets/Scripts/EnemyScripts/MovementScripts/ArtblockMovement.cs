926ac21ac91004d79e6788ca23f744d05adcc5ba
myong003 <matthew.yong0801@gmail.com>
2022-02-01 14:55:40 -0800

    Fixed artblock enemy

---
+++ Sketch/Assets/Scripts/EnemyScripts/MovementScripts/ArtblockMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtblockMovement : EnemyMovement
+{
+    public float slideSpeed = 2;
+    public float jumpHeight = 2;
+    protected Animator artblockAnim;
+    protected float slideTimer;
+    protected float direction;
+    protected int level;
+
+    protected override void move(){
+        Vector2 pos = gameObject.transform.position;
+        direction = -direction;
+        enemyRigidBody.velocity = new Vector2(direction, jumpHeight);
+        if (direction < 0){
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else{
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        slideTimer = 0;
+    }
+    protected void moveTowardsPlayer(){
+        Vector2 pos = gameObject.transform.position;
+        center = amPlayer.transform.position;
+        if (pos.x < center.x){
+            direction = maxSpeed;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else{
+            direction = -maxSpeed;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        enemyRigidBody.velocity = new Vector2(direction, jumpHeight);
+        slideTimer = 0;
+    }
+    protected override void Start()
+    {
+        base.Start();
+        artblockAnim = gameObject.GetComponent<Animator>();
+        slideTimer = slideSpeed;
+        direction = maxSpeed;
+        level = gameObject.GetComponent<EnemyCombat>().level;
+    }
+    protected override void Update()
+    {
+        Vector2 positionDifference = gameObject.transform.position - amPlayer.transform.position;
+        float playerDistance = positionDifference.magnitude;
+        if (gameObject.GetComponent<EnemyCombat>().isStunned()){
+            moveSpeed = 0;
+        }
+        else if (slideTimer >= slideSpeed){
+            moveSpeed = maxSpeed;
+            if (level == 1){
+                artblockAnim.Play("artblock1move");
+            }
+            else if (level == 2){
+                artblockAnim.Play("artblock2jump");
+            }
+
+            if (playerDistance < targetDistance){
+                moveTowardsPlayer();
+            }
+            else{
+                move();
+            }
+        }
+        else if (slideTimer < slideSpeed){
+            slideTimer += Time.deltaTime;
+        }
+    }
+}
