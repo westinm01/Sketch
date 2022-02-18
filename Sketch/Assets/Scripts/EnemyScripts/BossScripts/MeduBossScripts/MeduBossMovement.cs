9cd6c817f91e9bb60e0da45714e54b8882dc07f8
myong003 <matthew.yong0801@gmail.com>
2022-02-17 23:37:44 -0800

    Added in the new spritesheet

--- Sketch/Assets/Scripts/EnemyScripts/BossScripts/MeduBossScripts/MeduBossMovement.cs
+++ Sketch/Assets/Scripts/EnemyScripts/BossScripts/MeduBossScripts/MeduBossMovement.cs
@@ -6,15 +6,20 @@ public class MeduBossMovement : MonoBehaviour
 {
     [SerializeField] private float jumpHeight;
     [SerializeField] private float attackTime;
+    [SerializeField] private float crouchTime;
 
     private float attackTimer;
+    private float crouchTimer;
     private Animator anim;
     private Rigidbody2D rb;
+    private BossCombat combat;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
         rb = gameObject.GetComponent<Rigidbody2D>();
+        combat = gameObject.GetComponentInChildren<BossCombat>();
         attackTimer = 0;
     }
 
@@ -31,6 +36,9 @@ public class MeduBossMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (combat == null){
+            Destroy(this.gameObject);
+        }
         if (attackTimer >= attackTime){
             // Jump();
             Slap();
