cc2ecc989f7a3985afb63593a51f6118dd9dcd47
myong003 <matthew.yong0801@gmail.com>
2022-02-19 23:32:42 -0800

    Fixed the medu slap

--- Sketch/Assets/Scripts/EnemyScripts/BossScripts/MeduBossScripts/MeduBossMovement.cs
+++ Sketch/Assets/Scripts/EnemyScripts/BossScripts/MeduBossScripts/MeduBossMovement.cs
@@ -8,7 +8,11 @@ public class MeduBossMovement : MonoBehaviour
     [SerializeField] private float attackTime;
     [SerializeField] private float crouchTime;
     [SerializeField] private GameObject leftHand;
+    [SerializeField] private Vector3 leftHandPos;
     [SerializeField] private GameObject rightHand;
+    [SerializeField] private Vector3 rightHandPos;
+    [SerializeField] private Transform groundPos;
+
 
     private float attackTimer;
     private float crouchTimer;
@@ -31,9 +35,35 @@ public class MeduBossMovement : MonoBehaviour
     }
 
     private void Slap(){
-        gameObject.transform.rotation = Quaternion.Euler(0, Random.Range(0, 2) * 180, 0);
         anim.Play("MeduSlapDown");
         crouchTimer = 0;
+        int randSlap = Random.Range(0, 2);
+        switch (randSlap){
+            case 0:
+                StartCoroutine(SlapRight());
+                break;
+            case 1:
+                StartCoroutine(SlapRight());
+                break;
+            default:
+                break;
+        }
+    }
+
+    IEnumerator SlapRight(){
+        // gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+        while (rightHand.transform.position != groundPos.transform.position){
+            rightHand.transform.position = Vector2.MoveTowards(rightHand.transform.position, groundPos.transform.position, 6 * Time.deltaTime);
+            yield return null;
+        }
+        Debug.Log("Right hand has reached new pos");
+        yield return new WaitForSeconds(crouchTime);
+        Debug.Log("Done waiting");
+        while (rightHand.transform.position != rightHandPos){
+            rightHand.transform.position = Vector2.MoveTowards(rightHand.transform.position, leftHandPos, 6 * Time.deltaTime);
+            yield return null;
+        }
+        Debug.Log("Right hand has returned to initial position");
     }
 
     // Update is called once per frame
@@ -55,8 +85,8 @@ public class MeduBossMovement : MonoBehaviour
         }
 
         if (attackTimer >= attackTime){
-            Jump();
-            // Slap();
+            // Jump();
+            Slap();
             attackTimer = 0;
         }
         else{
