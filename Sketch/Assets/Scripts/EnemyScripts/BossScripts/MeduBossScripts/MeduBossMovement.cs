974fb288a2a2e9a241e2f7ce5061a7fd3f0c4740
myong003 <matthew.yong0801@gmail.com>
2022-02-22 21:43:17 -0800

    Added screen shaking to medu boss

--- Sketch/Assets/Scripts/EnemyScripts/BossScripts/MeduBossScripts/MeduBossMovement.cs
+++ Sketch/Assets/Scripts/EnemyScripts/BossScripts/MeduBossScripts/MeduBossMovement.cs
@@ -40,6 +40,7 @@ public class MeduBossMovement : MonoBehaviour
         anim.Play("MeduStomp");
         rb.velocity = new Vector2(0, jumpHeight);
         Invoke("CreateShockwaves", 0.5f);
+        Invoke("CallShakeScreen", 0.5f);
     }
 
     private void CreateShockwaves(){
@@ -50,14 +51,29 @@ public class MeduBossMovement : MonoBehaviour
         rightShock.direction = new Vector2(1, 0);
     }
 
-    private void ShakeScreen(){
-
+    private void CallShakeScreen(){
+        StartCoroutine(ShakeScreen());
+    }
+    IEnumerator ShakeScreen(){
+        Rigidbody2D cambody = cam.GetComponent<Rigidbody2D>();
+        float shakeTimer = 0;
+        float shakeTime = 1;
+        Vector2 direction =new Vector2(0, -1);
+        while (shakeTimer < shakeTime){
+            cambody.velocity = direction;
+            direction = -direction;
+            shakeTimer += Time.deltaTime;
+            yield return null;
+        }
+        cambody.velocity = Vector2.zero;
+        cam.transform.position = new Vector3(0, 0, -10);
     }
 
     private void Slap(){
         // gameObject.transform.rotation = Quaternion.Euler(0, Random.Range(0, 2) * 180, 0);
         anim.Play("MeduSlapDown");
         crouchTimer = 0;
+        Invoke("CallShakeScreen", 0.5f);
         StartCoroutine(SlapRight());
     }
     IEnumerator SlapRight(){
@@ -110,9 +126,9 @@ public class MeduBossMovement : MonoBehaviour
                     attackPhase = 2;
                     break;
                 case 2:
-                    Jump();
-                    // Slap();
-                    // gameObject.GetComponent<MeduSpawnEnemy>().spawnEnemy();
+                    // Jump();
+                    Slap();
+                    gameObject.GetComponent<MeduSpawnEnemy>().spawnEnemy();
                     attackPhase = 0;
                     break;
                 default:
