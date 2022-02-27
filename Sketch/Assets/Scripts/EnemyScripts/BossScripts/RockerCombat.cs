7ee1b570b3f1d410cd5b81230dfd85b137e9a05b
Wes <wmont012@ucr.edu>
2022-02-27 02:55:05 -0800

    Rocker is pretty much done!
    Now we just need the last 30 seconds, but besides that, it's done!

--- Sketch/Assets/Scripts/EnemyScripts/BossScripts/RockerCombat.cs
+++ Sketch/Assets/Scripts/EnemyScripts/BossScripts/RockerCombat.cs
@@ -7,17 +7,24 @@ public class RockerCombat : BossCombat
     // Start is called before the first frame update
     public GameObject speaker;
     
-    
+    protected override void Update(){
+        base.Update();
+        if(this.GetComponent<Transform>().position.y>1.1f){
+            speaker.GetComponent<Transform>().position=new Vector3(6.69f,-3.01f,1f);
+        }
+
+    }
      public override void bossTakeDamage(Rigidbody2D playerRigidBody)
     {
         
-        //Vector2 ve=new Vector2(0,600);
-        //enemyRigidBody.AddForce(ve);
-        this.GetComponent<Transform>().position=new Vector3(6.34f,1f,0f);
+        Vector2 ve=new Vector2(0,475);
+        enemyRigidBody.AddForce(ve);
+        //this.GetComponent<Transform>().position=new Vector3(6.34f,1f,0f);
+        //this.transform.GetChild(0).GetComponent<Transform>().position=new Vector3(0.23f,-1.39f,0f);
         //Vector2 v = new Vector2(-1000000,0);
         //speaker.GetComponent<Rigidbody2D>().AddForce(v);//AddForce(v);
         //jump
-        speaker.GetComponent<Transform>().position=new Vector3(6.69f,-3.01f,1f);
+        
         base.bossTakeDamage(playerRigidBody);
         //Debug.Log("Jump");
         
--- Sketch/Assets/Scripts/ResetBool.cs
+++ Sketch/Assets/Scripts/ResetBool.cs
@@ -6,10 +6,6 @@ public class ResetBool : StateMachineBehaviour
 {
     [SerializeField] private string boolName;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
