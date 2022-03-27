1505582ccdfd748cd3cf75318cee66dbead4f653
brentnicks <48264066+brentnicks@users.noreply.github.com>
2022-03-26 19:25:32 -0700

    othal boss

--- Sketch/Assets/Scripts/EnemyScripts/BossScripts/OthalBoss/OthalBoss.cs
+++ Sketch/Assets/Scripts/EnemyScripts/BossScripts/OthalBoss/OthalBoss.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class OthalBoss : BossCombat
 {
+    public GameObject gameManager;
     public GameObject[] tiles;
     public GameObject currTile;
     public Animator anim;
@@ -13,11 +14,16 @@ public class OthalBoss : BossCombat
     int counter = 0;
     bool canChangeTile = true;
 
+    protected override void Start()
+    {
+        maxHealth = health;
+        StartCoroutine(MusicSync());
+    }
     protected override void Update()
     {
         if (timer >= delayTime && canChangeTile)
         {
-            StartCoroutine(MusicSync());
+            //StartCoroutine(AttackSync());
             timer = 0;
             ++counter;
         }
@@ -48,11 +54,86 @@ public class OthalBoss : BossCombat
         currTile.SetActive(true);
     }
 
-    public IEnumerator MusicSync()
+    public IEnumerator AttackSync()
     {
         anim.Play("Bang");
-        yield return new WaitForSeconds(0.6f);
+        yield return new WaitForSeconds(0.28f);
         changeTile();
-        StopAllCoroutines();
+        StopCoroutine(AttackSync());
+    }
+
+    public IEnumerator MusicSync()
+    {
+        yield return new WaitForSeconds(7.57f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(3.73f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(3.75f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(0.95f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(1.3f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(0.94f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(1.87f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(0.96f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(0.93f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(1.88f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(0.92f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(0.96f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(1.86f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(0.95f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(0.92f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(1.88f); //30.07s
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(3.74f);
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(3.78f); //37.59s
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(3.73f); //41.32s
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(3.75f); //45.07
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(3.76f); //48.83
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(3.74f); //52.57
+        StartCoroutine(AttackSync());
+
+        yield return new WaitForSeconds(3f);
+        gameManager.GetComponent<AudioSource>().enabled = false;
+        canChangeTile = false;
+        Destroy(GameObject.Find("Barrier"));
+        anim.Play("Tired");
     }
 }
