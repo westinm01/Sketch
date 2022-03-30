2b03001daa76de7244b5c14735426ca69047276d
myong003 <matthew.yong0801@gmail.com>
2022-03-29 22:31:16 -0700

    Fixed achievements not saving

--- Sketch/Assets/Scripts/DataScripts/Data.cs
+++ Sketch/Assets/Scripts/DataScripts/Data.cs
@@ -10,7 +10,18 @@ public class Data
     public bool[] levelBool = { false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false };  // each index represents a level corresponding with the build manager index list.
     public int health = 3;
     public bool[] bossBool = { false, false, false, false, false, false, false, false, false, false, false, false };
-    public bool[,] achievementBool = {{false, false, false, false}};
+    public bool[,] achievementBool = {{false, false, false, false}, {false, false, false, false}, {false, false, false, false},   // Othal
+                                            {false, false, false, false}, {false, false, false, false}, {false, false, false, false},    // Roc
+                                            {false, false, false, false}, {false, false, false, false}, {false, false, false, false},    // Medu
+                                            {false, false, false, false}, {false, false, false, false}, {false, false, false, false},    // Pitu
+                                            {false, false, false, false}, {false, false, false, false}, {false, false, false, false},    // Ippoc
+                                            {false, false, false, false}, {false, false, false, false}, {false, false, false, false},    // Dal
+                                            {false, false, false, false}, {false, false, false, false}, {false, false, false, false},    // Tempra
+                                            {false, false, false, false}, {false, false, false, false}, {false, false, false, false},    // Wer
+                                            {false, false, false, false}, {false, false, false, false}, {false, false, false, false},    // Thala
+                                            {false, false, false, false}, {false, false, false, false}, {false, false, false, false},    // Ine
+                                            {false, false, false, false}, {false, false, false, false}, {false, false, false, false},    // Occi
+                                            {false, false, false, false}, {false, false, false, false}, {false, false, false, false}};   // Po
     
 
     public Data(int[] levelInts, bool[] levelBools, int healths, bool[] bossBools, bool[,] achievementBools)
