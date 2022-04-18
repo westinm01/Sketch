4ff8bc6ce7e7d35874bf53e72746a1417f07db54
myong003 <matthew.yong0801@gmail.com>
2022-04-18 01:31:23 -0700

    Added basic keybind changing
    jump, switchmode, and erase block

--- Sketch/Assets/Scripts/DataScripts/StaticControls.cs
+++ Sketch/Assets/Scripts/DataScripts/StaticControls.cs
@@ -35,19 +35,38 @@ public static class StaticControls
         InitializeDictionary();
     }
 
-    private static void InitializeDictionary(){
+    public static void InitializeDictionary(){
         keyMapping = new Dictionary<string, KeyCode>();
         for (int i=0; i < keyMaps.Length; i++){
-            keyMapping.Add(keyMaps[i], defaults[i]);
+            if (!PlayerPrefs.HasKey(keyMaps[i])){
+                PlayerPrefs.SetString(keyMaps[i], defaults[i].ToString());
+            }
+            KeyCode currCode = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(keyMaps[i]));  // Convert saved playerPref string to keyCode
+            keyMapping.Add(keyMaps[i], currCode);
         }
     }
 
-    public static void SetKeyMap(string keyMap, KeyCode key){
+    public static void ResetToDefault(){
+        for (int i=0; i < keyMaps.Length; i++){
+            PlayerPrefs.SetString(keyMaps[i], defaults[i].ToString());
+        }
+        InitializeDictionary();
+    }
+
+    public static KeyCode GetKeyCode(string keyMap){
+        return keyMapping[keyMap];
+    }
+
+    public static int SetKeyMap(string keyMap, KeyCode key){
         if (!keyMapping.ContainsKey(keyMap)){
             Debug.Log("invalid keymap");
+            return -1;
             // throw new ArgumentException("Invalid KeyMap in SetKeyMap: " + keyMap);
         }
         keyMapping[keyMap] = key;
+        PlayerPrefs.SetString(keyMap, key.ToString());
+        Debug.Log(keyMap + " was successfully rebinded to " + key.ToString());
+        return 0;
     }
 
     public static bool GetKeyDown(string keyMap){
