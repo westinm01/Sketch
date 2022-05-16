fde9698982945191efa89864e4d1e57bc8c8af8a
myong003 <matthew.yong0801@gmail.com>
2022-05-16 02:10:19 -0700

    volume saves

--- Sketch/Assets/Scripts/UIScripts/MainMenu.cs
+++ Sketch/Assets/Scripts/UIScripts/MainMenu.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     public AudioMixer mixer;
     public GameObject _MainMenu;
     public GameObject LevelSelect;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    public TextMeshProUGUI currentResolutionText;
     public static bool goToLevelSelect = false;
+
 
     void OnEnable(){
         if (goToLevelSelect){
             LoadLevelSelect();
             goToLevelSelect = false;
         }
+        StartCoroutine(InitializeSettings());
     }
+
+    private IEnumerator InitializeSettings(){
+        yield return 0;
+        float musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+        float sfxVolume = PlayerPrefs.GetFloat("SFXVolume");
+        musicSlider.value = musicVolume;
+        sfxSlider.value = sfxVolume;
+        musicSetVolume(musicSlider.value);
+        sfxSetVolume(sfxSlider.value);
+
+        currentResolutionText.text = Screen.currentResolution.width + " x " + Screen.currentResolution.height;
+    }
+
     public void PlayGame()
     {
         int totalLevelsDone = 0;
@@ -38,15 +58,21 @@ public class MainMenu : MonoBehaviour
     }
 
     public void SetVolume(float volume){
-        mixer.SetFloat("MasterVolume", volume);
+        float newVolume = Mathf.Log10(volume) * 20;
+        mixer.SetFloat("MasterVolume", newVolume);
+        PlayerPrefs.SetFloat("MasterVolume", volume);
     }
 
     public void musicSetVolume(float volume){
-        mixer.SetFloat("Music Volume", Mathf.Log10(volume) * 20);
+        float newVolume = Mathf.Log10(volume) * 20;
+        mixer.SetFloat("Music Volume", newVolume);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
     }
 
     public void sfxSetVolume(float volume){
-        mixer.SetFloat("Sound Effect", Mathf.Log10(volume) * 20);
+        float newVolume = Mathf.Log10(volume) * 20;
+        mixer.SetFloat("Sound Effect", newVolume);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
     }
 
     public void SetFullscreen(bool isFullscreen){
@@ -65,26 +91,21 @@ public class MainMenu : MonoBehaviour
         int maxWidth = resolutions[resolutions.Length - 1].width;
         switch (option){
             case 0:
-                
                 if (maxWidth < 2560) break;
-
                 Screen.fullScreenMode = FullScreenMode.Windowed;
                 SetResolutionScale(2560);
                 break;
             case 1:
-
                 if (maxWidth < 1920) break;
                 Screen.fullScreenMode = FullScreenMode.Windowed;
                 SetResolutionScale(1920);
                 break;
             case 2:
-
                 if (maxWidth < 1600) break;
                 Screen.fullScreenMode = FullScreenMode.Windowed;
                 SetResolutionScale(1600);
                 break;
             case 3:
-
                 if (maxWidth < 1280) break;
                 Screen.fullScreenMode = FullScreenMode.Windowed;
                 SetResolutionScale(1280);
